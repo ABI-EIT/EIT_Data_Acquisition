f911cc8c33da501e1c1a5535120c621886f17358
acreegan <andrew.s.creegan@gmail.com>
2020-11-17 18:14:52 +1300

    Reader working, writer working, plotter not working yet

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import sys
 import numpy as np
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar
 )
+import matplotlib
 from pyeit.app.eit import *
 import pyvisa
+from thread_helpers.worker import Producer, Consumer
+import time
 
 Ui_MainWindow, QMainWindow = uic.loadUiType("layout/layout.ui")
 
-pickle = "configuration/mesha06_bumpychestslice_pickle"
-data = "data/201104_16Rlung.txt"
-conf = "configuration/conf.json"
-background = None
+
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -25,6 +25,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.toolbar = None
         self.fig = None
         self.populate_devices()
+        self.reader = Reader()
+        self.data_writer = DataWriter()
+        self.plotter = PlotterConsumer()
+        self.add_plot(matplotlib.figure.Figure())
+        self.pickle = "configuration/mesha06_bumpychestslice_pickle"
+        self.conf = "configuration/conf.json"
+        self.background = None
 
     def add_plot(self, fig):
         self.fig = fig
@@ -35,12 +42,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.verticalLayout.addWidget(self.toolbar)
         self.canvas.draw()
 
-        self.device = None
-
-    def change_plot(self):
-        self.fig.clear()
-        ax = fig.subplots()
-        ax.plot(np.random.rand(5))
+    def update_plot(self, fig):
+        self.fig = fig
         self.canvas.draw()
 
     def populate_devices(self):
@@ -48,17 +51,96 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.comboBox.addItems(self.rm.list_resources())
 
     def change_device(self, text):
+        self.reader.start_new(on_start_args=(text,), work_args=(), on_stopped_args=())
+        self.reader.add_subscriber(self.data_writer.queue)
+        self.data_writer.start_new(on_stopped_args="", work_args="", on_start_args="")
+        self.data_writer.new_data.connect(lambda data: self.textEdit.append(data))
+        self.plotter.start_new(on_start_args=(self.pickle,self.conf,self.background), work_args=(self.update_plot,),on_stopped_args=())
+        self.reader.add_subscriber(self.plotter.queue)
+
+
+class Reader(Producer, QtCore.QObject):
+    state_signal = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        Producer.__init__(self)
+        QtCore.QObject.__init__(self)
+        self.device = None
+
+    def on_start(self, device_name):
+        rm = pyvisa.ResourceManager()
+        self.device = rm.open_resource(device_name)
+        self.device.baud_rate = 115200
+
+    def on_stopped(self, on_stopped_message):
+        pass
+
+    def producer_work(self, *args):
         try:
-            self.device = self.rm.open_resource(text)
+            while self.device.bytes_in_buffer < 2048:
+                time.sleep(0.01)
+            data = self.device.read()
         except pyvisa.errors.VisaIOError as e:
             print(e)
+            return None
+        return data
 
-    def read(self):
-        if self.device is not None:
-            try:
-                self.textEdit.append(self.device.read())
-            except pyvisa.errors.VisaIOError as e:
-                print(e)
+    def on_state_changed(self, state):
+        self.state_signal.emit(state)
+
+
+class DataWriter(Consumer, QtCore.QObject):
+    state_signal = QtCore.pyqtSignal(str)
+    new_data = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        Consumer.__init__(self)
+        QtCore.QObject.__init__(self)
+
+    def on_start(self, *args):
+        pass
+
+    def on_stopped(self, *args):
+        pass
+
+    def consumer_work(self, item, *args):
+        if item is not None:
+            self.new_data.emit(item)
+
+
+class PlotterConsumer(Consumer, QtCore.QObject):
+    state_signal = QtCore.pyqtSignal(str)
+    new_data = QtCore.pyqtSignal(str)
+
+    def __init__(self):
+        Consumer.__init__(self)
+        QtCore.QObject.__init__(self)
+
+    def on_start(self, pickle, conf, background):
+        self.eit_obj = unpickle_eit(pickle)
+        self.conf = load_conf(conf)
+        if background is not None:
+            self.background = load_oeit_data(background)[0]
+        else:
+            self.background = None
+
+    def on_stopped(self, *args):
+        pass
+
+    def consumer_work(self, item, *args):
+        if item is not None:
+            data = []
+            for line in item:
+                data.append(parse_oeit_line(line))
+
+            print(data)
+
+            if data[0] is not None:
+                print("Got a frame")
+                eit_image = process_frame(self.eit_obj, data[0], self.conf, self.background)
+                fig, imgs = create_plot([eit_image], self.eit_obj)
+
+                args[0](fig)
 
 
 if __name__ == '__main__':
@@ -68,19 +150,6 @@ if __name__ == '__main__':
 
     main_window.resize(dw.availableGeometry(dw).size() * 0.7)
 
-    eit_obj = unpickle_eit(pickle)
-    data = load_oeit_data(data)
-    conf = load_conf(conf)
-    if background is not None:
-        background = load_oeit_data(background)[0]
-    else:
-        background = None
-
-    eit_image = process_frame(eit_obj, data[0], conf, background)
-    fig, imgs = create_plot([eit_image], eit_obj)
-
-    main_window.add_plot(fig)
-    main_window.pushButton.clicked.connect(main_window.read)
 
     main_window.comboBox.currentTextChanged.connect(main_window.change_device)
 
---
+++ thread_helpers/worker.py
@@ -0,0 +1,99 @@
+import threading
+import time
+import queue
+from abc import ABCMeta, abstractmethod
+
+
+class Worker:
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        self.worker_thread = None
+        self.state = "Stopped"
+        self.lock = threading.Lock()
+
+    def set_stopped(self):
+        self.lock.acquire()
+        self.state = "Stopped"
+        self.lock.release()
+
+    def get_state(self):
+        self.lock.acquire()
+        state = self.state
+        self.lock.release()
+        return state
+
+    def start_new(self, on_start_args, work_args, on_stopped_args):
+        if self.worker_thread is not None:
+            self.set_stopped()
+            while self.worker_thread.is_alive():
+                time.sleep(0)
+        self.state = "Started"
+        self.worker_thread = threading.Thread(target=self.work, args=(on_start_args, work_args, on_stopped_args), daemon=True).start()
+
+    @abstractmethod
+    def on_state_changed(self, state):
+        pass
+
+    @abstractmethod
+    def work(self, *args):
+        pass
+
+    @abstractmethod
+    def on_start(self, *args):
+        pass
+
+    @abstractmethod
+    def on_stopped(self, *args):
+        pass
+
+
+class Producer(Worker):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super().__init__()
+        self.queues = []
+
+    def add_subscriber(self, queue):
+        self.queues.append(queue)
+
+    def work(self, on_start_args, work_args, on_stopped_args):
+        self.on_state_changed(self.get_state())
+        self.on_start(*on_start_args)
+        while self.state != "Stopped":
+            result = self.producer_work(*work_args)
+            for queue in self.queues:
+                queue.put(result)
+        self.on_state_changed(self.get_state())
+        self.on_stopped(*on_stopped_args)
+
+    @abstractmethod
+    def producer_work(self, *args):
+        # Gets called in loop. Use self.set_stopped() to stop
+        pass
+
+
+class Consumer(Worker):
+    __metaclass__ = ABCMeta
+
+    def __init__(self):
+        super().__init__()
+        self.queue = queue.Queue()
+
+    def work(self,  on_start_args, work_args, on_stopped_args):
+        self.on_state_changed(self.get_state())
+        self.on_start(*on_start_args)
+        while self.state != "Stopped":
+            item = self.queue.get()
+            self.consumer_work(item, *work_args)
+        self.on_state_changed(self.get_state())
+        self.on_stopped(*on_stopped_args)
+
+    @abstractmethod
+    def consumer_work(self, item, *args):
+        # Gets called in loop. Use self.set_stopped() to stop
+        pass
+
+    def on_state_changed(self, state):
+        pass
