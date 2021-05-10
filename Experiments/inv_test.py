c3bb6cc0fe43a22907a79d670a6cfc72b3bd8a09
Andrew <andrew.s.creegan@gmail.com>
2021-05-10 14:33:06 +1200

    adding inv test

---
+++ Experiments/inv_test.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy.linalg import inv
+import time
+
+iterations = 100
+
+invstart = time.time()
+np.random.seed(0)
+a = np.random.random((500,500))
+ainv = [inv(a) for _ in range(iterations)]
+invend = time.time()
+
+mulstart = time.time()
+np.random.seed(0)
+a = np.random.random((500,500))
+amul = [np.matmul(a, a) for _ in range(iterations)]
+mulend = time.time()
+
+print(f'inv took {invend-invstart:.2f} seconds')
+print(f'mul took {mulend-mulstart:.2f} seconds')
